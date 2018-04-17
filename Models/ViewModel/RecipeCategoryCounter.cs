e49177b9c06f0d6e0ca15497d7b63e8d6870a732
Jakub <jaqb_malinowski@yahoo.com>
2018-04-17 23:30:54 +0200

    find recipes by category, limit recipes, count recipes in category

---
+++ Models/ViewModel/RecipeCategoryCounter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RecipeStorageAPI.Models.ViewModel
+{
+    public class RecipeCategoryCounter
+    {
+        public RecipeCategory RecipeCategory { get; set; }
+        public int Amount { get; set; }
+
+        public RecipeCategoryCounter(RecipeCategory recipeCategory, int amount)
+        {
+            RecipeCategory = recipeCategory;
+            Amount = amount;
+        }
+    }
+}
