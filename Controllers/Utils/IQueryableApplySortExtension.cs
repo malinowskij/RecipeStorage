e19f8799821cd5dd43014c4feb3099ce31732590
Jakub <jaqb_malinowski@yahoo.com>
2018-04-18 20:18:49 +0200

    sorting and searching recipes

---
+++ Controllers/Utils/IQueryableApplySortExtension.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic;
+using System.Web;
+
+namespace RecipeStorageAPI.Controllers.Utils
+{
+    public static class IQueryableApplySortExtension
+    {
+        public static IQueryable<T> ApplySort<T>(this IQueryable<T> source, string strSort)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source", "DataSource is empty");
+
+            if (strSort == null)
+                return source;
+
+            var lstSort = strSort.Split(',');
+            string sortExpression = string.Empty;
+
+            foreach (var sortOption in lstSort) 
+            {
+                if (sortOption.StartsWith("-"))
+                {
+                    sortExpression += sortOption.Remove(0, 1) + " descending,";
+                } else
+                {
+                    sortExpression += sortOption + ",";
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(sortExpression))
+            {
+                source = source.OrderBy(sortExpression.Remove(sortExpression.Count() - 1));
+            }
+
+            return source;
+        }
+    }
+}
---
+++ Models/ViewModel/PagingParameterModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RecipeStorageAPI.Models.ViewModel
+{
+    public class PagingParameterModel
+    { 
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 12;
+    }
+}
